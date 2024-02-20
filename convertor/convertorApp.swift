170675fdafa9bd12bfdbd88b45c032399547cbbc
igorrflorentino <110570918+igorrflorentino@users.noreply.github.com>
2024-02-20 11:06:23 -0300

    Initial Commit

---
+++ convertor/convertorApp.swift
@@ -0,0 +1,17 @@
+//
+//  convertorApp.swift
+//  convertor
+//
+//  Created by Igor Florentino on 20/02/24.
+//
+
+import SwiftUI
+
+@main
+struct convertorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
